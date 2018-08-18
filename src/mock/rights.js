831e645e7767d3c58160aae2fd673fa8d95fce91
王焱辉 <wangyanhui@wangyanhuideMacBook-Pro.local>
2018-08-18 14:52:13 +0800

    修改路径

--- src/mock/rights.js
+++ src/mock/rights.js
@@ -17,7 +17,7 @@ let arr = [{
 }]
 
 //查询
-Mock.mock(/\/api\/orga\/list/, 'get', function (options) {
+Mock.mock(/\/api\/rights\/list/, 'get', function (options) {
     const Name = getParam(options.url,'Name')
     if(Name){
         return _.filter(arr,item=>item.Name.indexOf(Name)>-1)
@@ -27,7 +27,7 @@ Mock.mock(/\/api\/orga\/list/, 'get', function (options) {
 })
 
 //新增
-Mock.mock('/api/orga/add', 'post', function (options) {
+Mock.mock('/api/rights/add', 'post', function (options) {
     let info = JSON.parse(options.body)
     info.ID = Mock.Random.id()
     info.CreateTime = Mock.Random.date()
@@ -37,7 +37,7 @@ Mock.mock('/api/orga/add', 'post', function (options) {
 })
 
 //修改
-Mock.mock('/api/orga/edit', 'post', function (options) {
+Mock.mock('/api/rights/edit', 'post', function (options) {
     let info = JSON.parse(options.body)
     let origin = _.find(arr,(item)=>(item.ID === info.ID))
     let updated = _.assign(origin,info)
@@ -45,7 +45,7 @@ Mock.mock('/api/orga/edit', 'post', function (options) {
 })
 
 //删除
-Mock.mock('/api/orga/delete', 'post', function (options) {
+Mock.mock('/api/rights/delete', 'post', function (options) {
     let ID = JSON.parse(options.body).ID
     _.remove(arr, item => (
         item.ID === ID
